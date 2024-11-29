e2d897a3e14f9862e9dbd571ba6cd0b875e21f8e
joona <joona@Ubuntu22.myguest.virtualbox.org>
2024-11-29 18:02:03 +0200

    up

--- test1.sql
+++ test1.sql
@@ -1,4 +1,2 @@
 INSERT INTO areas (id, name, is_private) VALUES (100, 'testGeneral Discussion', FALSE);
-INSERT INTO threads (id, title, area_id) VALUES (100, 'Test 1', 1);
 INSERT INTO areas (id, name, is_private) VALUES (200, 'testBug Reports', FALSE);
-INSERT INTO threads (id, title, area_id) VALUES (200, 'Test 2', 2);
