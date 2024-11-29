a42358922db302ae5bd6cb021aa1a7b27a1147b6
joona <joona@Ubuntu22.myguest.virtualbox.org>
2024-11-29 17:55:39 +0200

    update

--- test1.sql
+++ test1.sql
@@ -1,4 +1,4 @@
-INSERT INTO areas (id, name, is_private) VALUES (1, 'testGeneral Discussion', FALSE);
-INSERT INTO threads (id, title, area_id) VALUES (1, 'Test 1', 1);
-INSERT INTO areas (id, name, is_private) VALUES (2, 'testBug Reports', FALSE);
-INSERT INTO threads (id, title, area_id) VALUES (2, 'Test 2', 2);
+INSERT INTO areas (id, name, is_private) VALUES (100, 'testGeneral Discussion', FALSE);
+INSERT INTO threads (id, title, area_id) VALUES (100, 'Test 1', 1);
+INSERT INTO areas (id, name, is_private) VALUES (200, 'testBug Reports', FALSE);
+INSERT INTO threads (id, title, area_id) VALUES (200, 'Test 2', 2);
